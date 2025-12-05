58fab83ae6b9768673d73bc94b5e2c2cbfc1b10a
Abhilashpendyala <abhilashpendyala142@gmail.com>
2025-12-05 14:06:41 +0530

    making flask run across network interfaces

--- flask_local.py
+++ flask_local.py
@@ -3,9 +3,13 @@ from flask import Flask
 #__name__ is default variable in python, set to "main" when the script is run directly, or else will
 # set to the "file name/module" name
 
-app=Flask(__name__)  # creates instance of web application
+app=Flask(__name__)  # creates instance of web application and tells flask from where to fetch the info
+
+app.run(host="0.0.0.0",port=5000) #0.0.0.0 tells flask to bind all network interfaces 
+#this makes ur code to accept connections from any ip on your netorks if reachable
 
 @app.route("/")
 def printfun():
-    return "<p>webserver running</p>"
+    return "<p>webserver running</p>"   #serves static html file
 
+## Run the script in terminal using "flask --app flask_local run"
