8d7efb12ee1e066525fb28a66b6aba94d39d086f
Abhilashpendyala <abhilashpendyala142@gmail.com>
2025-12-05 14:44:25 +0530

    running web server on single thread using flask for dev

--- flask_local.py
+++ flask_local.py
@@ -5,11 +5,14 @@ from flask import Flask
 
 app=Flask(__name__)  # creates instance of web application and tells flask from where to fetch the info
 
-app.run(host="0.0.0.0",port=5000) #0.0.0.0 tells flask to bind all network interfaces 
-#this makes ur code to accept connections from any ip on your netorks if reachable
+#app.run(host="0.0.0.0",port=5000) #0.0.0.0 tells flask to bind all network interfaces 
+#this makes ur code to accept connections from any ip on your netorks if reachable --> if running using script
+
+# flask --app flask_local run --host=0.0.0.0 --port=5000 ( if running flask from cli)
 
 @app.route("/")
 def printfun():
     return "<p>webserver running</p>"   #serves static html file
 
 ## Run the script in terminal using "flask --app flask_local run"
+#use nginix for static content files and gunicorn for app server in production
