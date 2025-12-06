cd1e5a7ef9475b18269ba288966350e415e68fe5
Abhilashpendyala <abhilashpendyala142@gmail.com>
2025-12-06 17:24:52 +0530

    intial commit with dependencies and first stage of work

---
+++ dependencies.py
@@ -0,0 +1,6 @@
+#flask  ---> runs local webserver 
+#django  ---> runs local app server
+#requests ---> API calls
+#Nginix ---> Real web server
+#Gunicorn ---> Real app server for deployment
+#Mysql , Redis, Mongo DB ---> Non SQL and SQL DBs
